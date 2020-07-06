15e567ac82ef0ea7d6f2dbcb980e47743b8e99a9
anavianm <anavian.max@gmail.com>
2020-07-06 14:08:25 -0400

    Started creating routes for a home page and a about page

---
+++ SimpactApp/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='simpact-home'),
+    path('about', views.simpactAbout, name='simpact-about'),
+]
--- SimpactApp/views.py
+++ SimpactApp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse('<doctype>...')
+
+def simpactAbout(request):
+    return HttpResponse('<h1> this is the about page <h1>')
 
 # Create your views here.
