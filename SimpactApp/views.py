48e882d00495bbf0ef4a4ac139ec3ebf4db2eeb9
Maxwell Anavian <anavian.max@gmail.com>
2020-07-06 21:00:37 -0400

    Routeupdates (#28)
    * added route to index.html at localhost/simpact
    
    * added app for users

--- SimpactApp/views.py
+++ SimpactApp/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('<doctype>...')
+    return render(request, 'Simpact HTML Files/index.html')
 
 def simpactAbout(request):
     return HttpResponse('<h1> this is the about page <h1>')
+
+
 
 # Create your views here.
--- simpact/settings.py
+++ simpact/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'SimpactApp.apps.SimpactappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
