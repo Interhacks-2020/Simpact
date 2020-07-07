2a207e95843e54818d6865d01237cfdf90e3e47e
Maxwell Anavian <anavian.max@gmail.com>
2020-07-06 23:23:20 -0400

    Routeupdates (#31)
    * added route to index.html at localhost/simpact
    
    * added app for users
    
    * added a route for user stuff

--- simpact/settings.py
+++ simpact/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'SimpactApp.apps.SimpactappConfig',
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
