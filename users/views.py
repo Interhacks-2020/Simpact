3213d539f8f18d00bd04f29cbd7d22f045901f82
anavianm <anavian.max@gmail.com>
2020-07-06 23:20:19 -0400

    added a route for user stuff

--- users/views.py
+++ users/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.contrib.auth.forms import UserCreationForm
+def register(request):
+    form = UserCreationForm()
+    return render(request, 'users/register.html', {'form': form})
