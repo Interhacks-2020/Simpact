820ad889d8f39482aae68d49719221e08bd506d8
Baffy <baffyntiamoah@gmail.com>
2020-07-08 19:36:35 -0400

    Populated the tables

---
+++ sql/006_volunteers_data.sql
@@ -0,0 +1,13 @@
+SELECT * FROM database_1.volunteers;
+INSERT INTO parents(volunteer_id, volunteer_name, hours_worked, telephone,email, points)
+VALUES (1,'KevinSaka',6,'85678534','K@gmail.com',200),
+       (2,'Eve Nun',8,'67578534','eve@gmail.com',450),
+       (3,'Danun',6,'678878777','danun@gmail.com',600),
+       (4,'Jane Fo',1,'7853445','jane@gmail.com',150),
+       (5,'Sam Pat',6,'8878534','sam@gmail.com',40),
+       (6,'Pete',9,'85690658','pete@gmail.com',78)
+       
+       
+        
+
+
