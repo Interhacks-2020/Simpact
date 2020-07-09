77b701fbe73ac16ff8ee28f8eda8d7fa9acda56a
Baffy <baffyntiamoah@gmail.com>
2020-07-08 21:10:06 -0400

    deleted the parent table due to wrong columns

--- sql/004_parents_data.sql
+++
@@ -1,9 +0,0 @@
-SELECT * FROM database_1.parents;
-INSERT INTO parents(idparents, telephone, email, parent_name,parents_child)
-VALUES (1,'54678647','elt@gmailcom','Elton Jo','duncun jo'),
-       (2,'89678647','su@gmailcom','Susan','olive'),
-       (3,'54678647','elt@gmailcom','Elton Jo','duncun jo')
-        
-        
-        
-       
