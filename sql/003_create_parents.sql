7877a4d35bb37b1ab3faed0b9e10f3bbc53f06c6
Baffy <baffyntiamoah@gmail.com>
2020-07-09 03:51:18 -0400

    deleted older files due to wrong queries

--- sql/003_create_parents.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `parents` (
-  `idparents` int NOT NULL,
-  `telephone` varchar(45) NOT NULL,
-  `email` varchar(45) NOT NULL,
-  `parent_name` varchar(45) NOT NULL,
-  `parents_child` varchar(45) NOT NULL,
-  PRIMARY KEY (`idparents`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
--- sql/007_non_profits_org_data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO non_profit_org  (n_name, n_city,n_services_offered,n_telephone, number_of_volunteers)
-VALUES ('Headler4','Denver','cooking','73875376',34),
-	    ('ZenGarden','Memphis','gardening','64763878',45),
-        ('FreeComm','Atlanta','donations for students', '45895986',67),
-        ('DrainOrg', 'Seattle','teaching kids math', '687867576',69)
