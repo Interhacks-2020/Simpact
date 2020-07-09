2aa966fd86bfb81c91960310843c1888e29d7d9d
Baffy <baffyntiamoah@gmail.com>
2020-07-08 20:09:09 -0400

    populated table for company

---
+++ sql/008_companies_data.sql
@@ -0,0 +1,5 @@
+INSERT INTO companies (c_id, c_name, services_offered, city, email,company_phone,number_of_volunteers,perks)
+VALUES ('CodePlusPLus','coding tutorials online','Medford','code@gmail.com','3674676',123,'monthly stipends'),
+       ('FoodKitchen', 'baking and pastry work','NewYork','food@gmail.com','46868656',200,'free health insurance'),
+       ('CleaningHomesCom', 'janitorial work', 'Boston','serv@gmail.com','476756846', 345,'free grocery bi weekly' ),
+       ('MediTate', 'making inspirational messages', 'Las Vegas','med@gmail.com','764868368', 348,'phone bill subsidiary')
