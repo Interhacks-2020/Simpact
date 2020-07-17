9af1f9e0ce4a037d88d3ab524cb5f44e2a85f423
anavianm <anavian.max@gmail.com>
2020-07-16 22:09:54 -0400

    added NPO route

--- SimpactApp/urls.py
+++ SimpactApp/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='simpact-home'),
     path('businessLogin', views.businessLogin, name='business-login'),
+    path('npoLogin', views.npoLogin, name='npo-login'),
     path('about', views.simpactAbout, name='simpact-about'),
     
     #path('', views.InsertPerks)
--- SimpactApp/views.py
+++ SimpactApp/views.py
@@ -7,6 +7,9 @@ def home(request):
 def businessLogin(request):
     return render(request, 'SimpactApp/businesslogin.html')
 
+def npoLogin(request):
+    return render(request, 'SimpactApp/npologin.html')
+
 def simpactAbout(request):
     return HttpResponse('<h1> this is the about page <h1>')
 
