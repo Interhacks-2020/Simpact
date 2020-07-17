2088c779affcb67c2c20a7ed351de81064532b48
Maxwell Anavian <anavian.max@gmail.com>
2020-07-16 21:51:57 -0400

    new routes! business button works correctly (#53)

--- SimpactApp/views.py
+++ SimpactApp/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'Simpact HTML Files/index.html')
+    return render(request, 'SimpactApp/index.html')
+
+def businessLogin(request):
+    return render(request, 'SimpactApp/businesslogin.html')
 
 def simpactAbout(request):
     return HttpResponse('<h1> this is the about page <h1>')
