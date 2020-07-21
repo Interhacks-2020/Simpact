b026ac75b17bff19d3cab19f87d5392d9f1c11ac
ankoanju <51080658+ankoanju@users.noreply.github.com>
2020-07-21 22:37:59 +0900

    Testing with volunter opportunities databases. (#59)

--- SimpactApp/views.py
+++ SimpactApp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import VolOpp
+
 def home(request):
     return render(request, 'SimpactApp/index.html')
 
@@ -21,7 +23,13 @@ def testPath(request):
     return HttpResponse('<h1> hello testing <h1>')
 
 
-
+#testing 
+def all_events(request):
+    event_list = VolOpp.objects.all()
+    return render(request,
+        'SimpactApp/volopp_list.html',
+        {'event_list': event_list}
+    )
 
 
 # Create your views here.
