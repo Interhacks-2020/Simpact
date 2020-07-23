67ff21bc586adaf485074f61d0df060cdd2e8db9
ankoanju <51080658+ankoanju@users.noreply.github.com>
2020-07-23 22:09:12 +0900

    Finalizing routes and everything for demo (#76)

--- SimpactApp/urls.py
+++ SimpactApp/urls.py
@@ -10,13 +10,20 @@ urlpatterns = [
     path('volLogin', views.volLogin, name='vol-login'),
     path('npodash', views.npoDash, name='npo-dash'),
     path('about', views.simpactAbout, name='simpact-about'),
-    path('opps/', views.all_events, name='show-events'),
+
     path('volListing/', views.all_volopps, name='vol-listing'),
+    path('volListing/voloppdescription', views.voloppdescription, name='volopp-description'),
+    path('volDashboard', views.volDashboard, name='vol-dash'),
+    
+    path('npoDashboard', views.npoDashboard, name='npo-dash'),
     path('add_volopp/', views.add_volopp, name='add-volopp'),
     
-    path('volListing/voloppdescription', views.voloppdescription, name='volopp-description'),
+    path('busPerks', views.busPerks, name='bus-perks'),
+    path('busNewPerks', views.busNewPerks, name='bus-newperks'),
+    path('busPromote', views.busPromote, name='bus-promote'),
     
-    #path('', views.InsertPerks)
+    #path('', views.InsertPerks),
+    path('opps/', views.all_events, name='show-events'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
